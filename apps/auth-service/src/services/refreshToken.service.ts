d863dfaa48b607bd0d4cf981a1c1496a4d19adee
Ansuman <ansumanp@mindfiresolutions.com>
2026-02-16 08:40:05 +0530

    feat: add test cases for refreshToken service

---
+++ apps/auth-service/src/services/refreshToken.service.ts
@@ -0,0 +1,6 @@
+import { RefreshTokenInput } from '../types/RefreshTokenInput';
+// import { RefreshTokenOutput } from "../types/RefreshTokenOutput";
+
+export const refreshToken = async ({ token }: RefreshTokenInput) => {
+  return token;
+};
