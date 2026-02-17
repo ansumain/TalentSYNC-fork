54672281a757e016832ac56a0e4fcaa70c35d0c6
Ansuman <ansumanp@mindfiresolutions.com>
2026-02-17 23:13:13 +0530

    feat: add test cases + controller for updatePassword

--- apps/auth-service/src/routes/userProfile.routes.ts
+++ apps/auth-service/src/routes/userProfile.routes.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { authMiddleware } from '../middlewares/auth.middleware';
 import { UserProfileController } from '../controllers/userProfile.controller';
 import { UpdateUserProfileController } from '../controllers/updateUserProfile.controller';
+import { UpdatePasswordController } from '../controllers/updatePassword.controller';
 const userProfileRouter = express.Router();
 
 userProfileRouter.get('/me', authMiddleware, UserProfileController.userProfile);
 userProfileRouter.put('/me', authMiddleware, UpdateUserProfileController.updateUserProfile);
+userProfileRouter.patch('/me/password', authMiddleware, UpdatePasswordController.updatePassword);
 
 export default userProfileRouter;
