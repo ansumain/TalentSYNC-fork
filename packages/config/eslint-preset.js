7a1a1bc8315b3a2e6cbaad4bc1911704339934ab
Ansuman <ansumanp@mindfiresolutions.com>
2026-02-23 11:06:30 +0530

    chore: modify package.json in root and services

---
+++ packages/config/eslint-preset.js
@@ -0,0 +1,15 @@
+export default {
+  extends: ['eslint:recommended'],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+  },
+};
