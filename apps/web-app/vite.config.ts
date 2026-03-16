f5db3953639f182bec59a2f86dec8805d5d40c96
Ansuman <ansumanp@mindfiresolutions.com>
2026-03-16 17:46:39 +0530

    chore: add vite bundle analyzer to web-app

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from "path";
+import { analyzer } from 'vite-bundle-analyzer';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    analyzer({
+      analyzerMode: 'static'
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
