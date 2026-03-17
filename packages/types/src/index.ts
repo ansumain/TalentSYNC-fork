f4655a34f9e0896b59b03ae44aec011927c581b1
Ansuman <ansumanp@mindfiresolutions.com>
2026-03-17 17:19:12 +0530

    feat: add global error handler types in package

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,1 +1,13 @@
 export type { UserInfo } from './UserInfo';
+export {
+	AppError,
+	badRequestError,
+	conflictError,
+	forbiddenError,
+	internalServerError,
+	isAppError,
+	notFoundError,
+	toApiErrorResponse,
+	unauthorizedError,
+} from './error';
+export type { ApiErrorPayload, ApiErrorResponse } from './error';
